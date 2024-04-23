67138893360d62c7071bf156b2cfa14474036f48
Deepak-Leo <151494157+Deepak-Leo@users.noreply.github.com>
2024-04-24 01:23:19 +0530

    Add files via upload

---
+++ Day_8/leoq3/src/main/java/com/example/leoq3/Leoq3Application.java
@@ -0,0 +1,13 @@
+package com.example.leoq3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leoq3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Leoq3Application.class, args);
+	}
+
+}
