d049a5df2f1035078be229239d6461aab8b681a3
Deepak-Leo <151494157+Deepak-Leo@users.noreply.github.com>
2024-04-24 01:18:38 +0530

    day 6

---
+++ Day_6/leoq1/src/main/java/com/example/leoq1/repository/ChildrenRepo.java
@@ -0,0 +1,9 @@
+package com.example.leoq1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.leoq1.model.Children;
+
+public interface ChildrenRepo extends JpaRepository<Children,Integer>{
+    
+}
